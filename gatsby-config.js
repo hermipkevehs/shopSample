6fb9b7a09a3d5f4e79507ffa4264372111159b38
Daisy Jane Batislaong <71486565+DaisyBatislaong@users.noreply.github.com>
2021-02-02 08:16:26 +0800

    first commit, setups

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+require("dotenv").config({
+   path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+   plugins: [
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      `gatsby-plugin-postcss`,
+      `gatsby-plugin-styled-components`,
+      {
+         resolve: `gatsby-source-contentful`,
+         options: {
+            spaceId: `c2jlpkowjo08`,
+            // Learn about environment variables: https://gatsby.dev/env-vars
+            accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+         },
+      },
+   ],
+};
