4230ae941deb72179b4cc26869c021cd6927959d
Daisy Jane Batislaong <71486565+DaisyBatislaong@users.noreply.github.com>
2021-02-20 20:12:48 +0800

    state successful

--- src/context/ProductProvider.js
+++ src/context/ProductProvider.js
@@ -9,7 +9,12 @@ export const ProductContext = createContext();
 const ProductReducer = (state, action) => {
    switch (action.type) {
       case "ONADD":
-         return {cartItems: [...state.cartItems, {payload: action.payload}] }
+         const exist = state.cartItems.find((x) => x.payload.products.slug === action.payload.products.slug);
+         if (exist) {
+            return { cartItems: state.cartItems.map((x) => (x.payload.products.slug === action.payload.products.slug ? { ...exist, qty: exist.qty + 1 } : x)) };
+         } else {
+            return { cartItems: [...state.cartItems, { payload: action.payload, qty: 1 }] };
+         }
       default:
          return state;
    }
