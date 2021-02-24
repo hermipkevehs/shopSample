7fef59687242a88df0c4dc0a33384c3b5d85dee0
Daisy Jane Batislaong <71486565+DaisyBatislaong@users.noreply.github.com>
2021-02-24 10:07:55 +0800

    change ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,12 @@ const React = require('react')
 const GlobalProvider = require('./src/context/GlobalProvider');
 const ProductProvider = require('./src/context/ProductProvider')
 
+exports.wrapPageElement = ({element}) => {
+  return(
+    <GlobalProvider><ProductProvider>{element}</ProductProvider></GlobalProvider>
+  )
+}
+
 exports.wrapRootElement = ({ element }) => {
    return (
      <GlobalProvider><ProductProvider>{element}</ProductProvider></GlobalProvider>
